d476da4e18bbfe58f30a2bcda7ec77f740b01690
varesc <varesc@gmail.com>
2018-05-20 09:52:25 +0700

    testing object

--- utils/utils.js
+++ utils/utils.js
@@ -3,3 +3,10 @@ module.exports.add = (a,b) => a+b;
 module.exports.square = (a) => a*a;
 
 
+module.exports.setName = (user,fullName) => {
+    var names = fullName.split(' ');
+    user.firstname = names[0];
+    user.lastname = names[1];
+    return user;
+
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,11 +1,16 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 
 it(`should add two numbers`,()=>{
     var res = utils.add(33,7);
-    if (res != 40){
-        throw new Error(`Expect 40 , the return value is ${res}`);
-    }
+    expect(res).toBe(40);
+    expect(res).toBeA('number');
+
+
+    // if (res != 40){
+    //     throw new Error(`Expect 40 , the return value is ${res}`);
+    // }
 
 });
 
@@ -13,8 +18,31 @@ it(`should add two numbers`,()=>{
 
 it(`should square a numbers`,()=>{
     var res = utils.square(2);
-    if (res != 4){
-        throw new Error(`Expect 4 , the return value is ${res}`);
-    }
+    expect(res).toBe(4).toBeA('number');
 
+});
+
+
+it(`should expect some values`,()=>{
+    var res = utils.square(2);
+    // expect(12).toNotBe(12);
+    // expect({name:"ming"}).toEqual({name:"ming"});
+    // expect(([1,2,3,4])).toInclude(4);
+    // expect(([1,2,3,4])).toExclude(5);
+    expect({
+        name:"ming",
+        age:26,
+        hobby:"programming"
+    }).toInclude({age:26});
+});
+
+
+
+it(`should verify that first and last name is correct`,()=>{
+    var user = {firstname:'',lastname:''};
+    var res = utils.setName(user,`Vares Chai`);
+    expect(res).toEqual({
+        firstname:"Vares",
+        lastname:"Chai"
+    }).toBeAn('object');
 });
